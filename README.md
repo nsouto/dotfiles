08340a63ef29dd2468d57c0c98d31be4c43641da
Nuno Souto <nsouto@lostlink.net>
2023-11-26 22:01:45 -0500

    Refactor file paths in scripts and Readme, and update .gitignore and config files for improved project structure consistency.

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Based on the repo at https://github.com/mattstauffer/dotfiles.git.
 1. Open Terminal
 1. [Create a GitHub personal access token for logging in from the command line.](https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/creating-a-personal-access-token). Give it just full `repo` access. Save that token in a text file or something.
 1. Type `git`, hit enter, and follow the prompts to install the command line developer tools
-1. Type `git clone https://github.com/nsouto/dotfiles.git ~/dotfiles`, hit enter
-1. Type `cd ~/dotfiles`, hit enter
+1. Type `git clone https://github.com/nsouto/dotfiles.git ~/Dotfiles`, hit enter
+1. Type `cd ~/Dotfiles`, hit enter
 1. Type `./bin/install.sh`, hit enter
 1. Profit?
 
--- support/symlinks.sh
+++ support/symlinks.sh
@@ -4,7 +4,7 @@ declare -A local_dotfile_symlinks
 declare -A remote_dotfile_symlinks
 declare -A remote_folder_symlinks
 
-local_dotfile_symlinks=([zshrc]=$HOME/.zshrc [vimrc]=$HOME/.vimrc [gitignore]=$HOME/.gitignore [lambo]=$HOME/.lambo)
+local_dotfile_symlinks=([zshrc]=$HOME/.zshrc [vimrc]=$HOME/.vimrc [gitignore]=$HOME/.gitignore [lambo]=$HOME/.lambo [aws]=$HOME/.aws [gitconfig]=$HOME/.gitconfig [gmvault]=$HOME/.gmvault [gnupg]=$HOME/.gnupg [zsh_history]=$HOME/.zsh_history)
 for key val in ${(kv)local_dotfile_symlinks}; do
     if test -f "$val"; then
         echo "$val already exists as a folder; deleting."
@@ -20,19 +20,19 @@ done
 
 # @todo: Make sure id_rsa and id_rsa exist *and* have the right permissions
 #        (600 for both)
-remote_dotfile_symlinks=([ssh]=$HOME/.ssh [aws]=$HOME/.aws [gitconfig]=$HOME/.gitconfig [gmvault]=$HOME/.gmvault [gnupg]=$HOME/.gnupg [zsh_history]=$HOME/.zsh_history)
-for key val in ${(kv)remote_dotfile_symlinks}; do
-    if test -f "$val"; then
-        echo "$val already exists as a folder; deleting."
-        rm -fr $val
-    fi
-    if [ -d "$val" ]; then
-        echo "$val already exists as a file; deleting."
-        rm $val
-    fi
-
-    ln -s $NAS_FOLDER/Dotfiles/$key $val
-done
+#remote_dotfile_symlinks=([ssh]=$HOME/.ssh)
+#for key val in ${(kv)remote_dotfile_symlinks}; do
+#    if test -f "$val"; then
+#        echo "$val already exists as a folder; deleting."
+#        rm -fr $val
+#    fi
+#    if [ -d "$val" ]; then
+#        echo "$val already exists as a file; deleting."
+#        rm $val
+#    fi
+#
+#    ln -s $NAS_FOLDER/Dotfiles/$key $val
+#done
 
 
 # @todo: Make sure Downloads, Sites and Temp are Synology Drives
